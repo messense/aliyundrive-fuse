35989ae560fcd7572fd161fdcc2a644b7ad0f763
messense <messense@icloud.com>
2022-01-09 20:41:12 +0800

    Clean cli options

--- src/main.rs
+++ src/main.rs
@@ -23,15 +23,9 @@ struct Opt {
     /// Working directory, refresh_token will be stored in there if specified
     #[clap(short = 'w', long)]
     workdir: Option<PathBuf>,
-    /// Delete file permanently instead of trashing it
-    #[clap(long, conflicts_with = "domain-id")]
-    no_trash: bool,
     /// Aliyun PDS domain id
     #[clap(long)]
     domain_id: Option<String>,
-    /// Enable read only mode
-    #[clap(long)]
-    read_only: bool,
 }
 
 fn main() -> anyhow::Result<()> {
@@ -44,29 +38,20 @@ fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
 
     let opt = Opt::parse();
-    let (drive_config, _no_trash) = if let Some(domain_id) = opt.domain_id {
-        (
-            DriveConfig {
-                api_base_url: format!("https://{}.api.aliyunpds.com", domain_id),
-                refresh_token_url: format!(
-                    "https://{}.auth.aliyunpds.com/v2/account/token",
-                    domain_id
-                ),
-                workdir: opt.workdir,
-                app_id: Some("BasicUI".to_string()),
-            },
-            true, // PDS doesn't have trash support
-        )
+    let drive_config = if let Some(domain_id) = opt.domain_id {
+        DriveConfig {
+            api_base_url: format!("https://{}.api.aliyunpds.com", domain_id),
+            refresh_token_url: format!("https://{}.auth.aliyunpds.com/v2/account/token", domain_id),
+            workdir: opt.workdir,
+            app_id: Some("BasicUI".to_string()),
+        }
     } else {
-        (
-            DriveConfig {
-                api_base_url: "https://api.aliyundrive.com".to_string(),
-                refresh_token_url: "https://websv.aliyundrive.com/token/refresh".to_string(),
-                workdir: opt.workdir,
-                app_id: None,
-            },
-            opt.no_trash,
-        )
+        DriveConfig {
+            api_base_url: "https://api.aliyundrive.com".to_string(),
+            refresh_token_url: "https://websv.aliyundrive.com/token/refresh".to_string(),
+            workdir: opt.workdir,
+            app_id: None,
+        }
     };
     let drive = AliyunDrive::new(drive_config, opt.refresh_token).map_err(|_| {
         io::Error::new(io::ErrorKind::Other, "initialize aliyundrive client failed")
@@ -74,9 +59,8 @@ fn main() -> anyhow::Result<()> {
 
     let vfs = AliyunDriveFileSystem::new(drive);
     let mut mount_options = vec![MountOption::AutoUnmount, MountOption::NoAtime];
-    if opt.read_only {
-        mount_options.push(MountOption::RO);
-    }
+    // read only for now
+    mount_options.push(MountOption::RO);
     if cfg!(target_os = "macos") {
         mount_options.push(MountOption::CUSTOM("-o local".to_string()));
     }
